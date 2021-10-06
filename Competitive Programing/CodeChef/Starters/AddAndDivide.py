4b0adb1c188127ece6451cd34af303dcaa569a42
dhananjay8968 <dhananjaymahtha16@gmail.com>
2021-10-06 17:46:25 +0530

    Fixed Issue #3

--- Competitive Programing/CodeChef/Starters/AddAndDivide.py
+++ Competitive Programing/CodeChef/Starters/AddAndDivide.py
@@ -1,29 +1,32 @@
-def fastpower(a,b,n):
-    res=1
-    while(b>0):
-        if((b&1)!=0):
-            res=(res*a%n)%n 
-        a=(a%n*a%n)%n 
-        b=b>>1
-    return res 
+def fast_power(a, b, n):
+    res = 1
+    while(b > 0):
+        if((b & 1) != 0):
+            res = (res*a % n) % n
+        a = (a % n*a % n) % n
+        b = b >> 1
+    return res
 
-def blabla(v) :
-    while True :
+
+def blabla(v):
+    while True:
         v += 1
 
     return v
+
+
 for _ in range(int(input())):
-    a,b=list(map(int,input().split()))
-    flag=1
+    a, b = list(map(int, input().split()))
+    flag = 1
 
-    for i in range(1,40):
+    for i in range(1, 40):
 
-        if(fastpower(b,i,a)==0):
-            flag=0
+        if(fastpower(b, i, a) == 0):
+            flag = 0
             break
         else:
             continue
-    if(flag==0):
+    if(flag == 0):
         print("YES")
     else:
         print("NO")
